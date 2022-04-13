adb7522ea508f423b5918b42a4a99b9aa3455b86
Tarmuji <tarmuji1514@gmail.com>
2022-04-13 22:59:43 +0700

    FIX: Hamburger Menu

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,10 +2,15 @@
 window.onscroll = function () {
   const header = document.querySelector("header");
   const fixedNav = header.offsetTop;
+  const toTop = document.getElementById("to-top");
 
   if (window.pageYOffset > fixedNav) {
     header.classList.add("navbar-fixed");
+    toTop.classList.remove("hidden");
+    toTop.classList.add("flex");
   } else {
+    toTop.classList.add("hidden");
+    toTop.classList.remove("flex");
     header.classList.remove("navbar-fixed");
   }
 };
@@ -17,4 +22,12 @@ const hamburger = document.getElementById("hamburger");
 hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("hamburger-active");
   navMenu.classList.toggle("hidden");
+});
+
+// Klik diluar Hamburger
+window.addEventListener("click", function (e) {
+  if (e.target != navMenu && e.target != hamburger) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+  }
 });
