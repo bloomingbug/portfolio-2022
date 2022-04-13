3f49ad7f5f4ae03b543dcc9a954a47b331609f36
Tarmuji <tarmuji1514@gmail.com>
2022-04-13 23:45:45 +0700

    FEAT: Dark Mode

--- dist/js/script.js
+++ dist/js/script.js
@@ -6,12 +6,14 @@ window.onscroll = function () {
 
   if (window.pageYOffset > fixedNav) {
     header.classList.add("navbar-fixed");
+    header.classList.remove("dark:bg-dark");
     toTop.classList.remove("hidden");
     toTop.classList.add("flex");
   } else {
+    header.classList.remove("navbar-fixed");
+    header.classList.add("dark:bg-dark");
     toTop.classList.add("hidden");
     toTop.classList.remove("flex");
-    header.classList.remove("navbar-fixed");
   }
 };
 
@@ -31,3 +33,28 @@ window.addEventListener("click", function (e) {
     navMenu.classList.add("hidden");
   }
 });
+
+// Toggle Darkmode
+const darkToggle = document.getElementById("dark-toggle");
+const html = document.querySelector("html");
+
+darkToggle.addEventListener("click", function () {
+  if (darkToggle.checked) {
+    html.classList.add("dark");
+    localStorage.theme = "dark";
+  } else {
+    html.classList.remove("dark");
+    localStorage.theme = "light";
+  }
+});
+
+// Pindah posisi toggle sesuai mode
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  darkToggle.checked = true;
+} else {
+  darkToggle.checked = false;
+}
